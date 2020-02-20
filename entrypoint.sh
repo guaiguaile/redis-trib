8aaaadc5a793b072fdf3fe0a18c1472bb8feb79c
L <13051960278@163.com>
2020-02-20 22:42:56 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 AUTH_PASSWORD=${AUTH_PASSWORD}
-
+CLIENT_PATH=$(find  / -name client.rb)
 if [ -z ${AUTH_PASSWORD} ];then 
   echo "If redis has authentication enabled, set it AUTH_PASSWORD"
 else
-  sed -i "s/:password => nil/:password => \"${AUTH_PASSWORD}\"/g" /usr/lib/ruby/gems/2.5.0/gems/redis-4.0.2/lib/redis/client.rb
+  sed -i "s/:password => nil/:password => \"${AUTH_PASSWORD}\"/g" ${CLIENT_PATH}
 fi
 
-exec "/usr/bin/redis-trib"
+exec "/usr/bin/redis-trib $@"
