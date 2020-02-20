0341bb5e9c43ba57afb2a68d0df723251b500327
L <13051960278@163.com>
2020-02-20 21:33:33 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,4 @@ else
   sed -i "s/:password => nil/:password => \"${AUTH_PASSWORD}\"/g" /usr/lib/ruby/gems/2.5.0/gems/redis-4.0.2/lib/redis/client.rb
 fi
 
-exec "/usr/bin/redis-trib $@"
+exec "/usr/bin/redis-trib"
